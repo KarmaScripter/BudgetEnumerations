4e8dbe0bae587c6d342605461e055899b89eef13
Terry D. Eppler <terry.eppler@gmail.com>
2021-10-15 20:03:12 -0400

    refactored directory structure;

---
+++ AwardType.cs
@@ -0,0 +1,35 @@
+﻿// <copyright file = "AwardType.cs" company = "Terry D. Eppler">
+// Copyright (c) Terry D. Eppler. All rights reserved.
+// </copyright>
+
+namespace BudgetExecution
+{
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public enum AwardType
+    {
+        /// <summary>
+        /// The ns
+        /// </summary>
+        NS = 0,
+
+        /// <summary>
+        /// The monetary
+        /// </summary>
+        Monetary,
+
+        /// <summary>
+        /// The time off
+        /// </summary>
+        TimeOff,
+
+        /// <summary>
+        /// The over time
+        /// </summary>
+        OverTime
+    }
+}
